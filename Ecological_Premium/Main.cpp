6a837c1e6bfdd3635affd195a0a066912b05fe59
LeoGMR20 <leo250703@gmail.com>
2022-10-07 23:12:56 -0400

    Commit 07/10/2022 23:12 (Leandro Morales) -> Fin del ejercicio 'Ecological_Premium'

--- Ecological_Premium/Main.cpp
+++ Ecological_Premium/Main.cpp
@@ -4,11 +4,13 @@ using namespace std;
 
 int main()
 {
-    int n, f, fy, a, fef;
+    int n, f;
 
     do{
         cin >> n;
     }while(n >= 20 || n <= 0);
+
+    int *res = new int[n];
 
     for (int i = 0; i < n; i++)
     {
@@ -16,19 +18,34 @@ int main()
             cin >> f;
         }while(f >= 20 || f <= 0);
 
-        int **fArray = new int*[4];
+        int **fArray = new int*[f];
 
-        for(int j = 0; j < 4; j++)
+        for(int j = 0; j < f; j++)
         {
-            fArray
+            fArray[j] = new int[3];
         }
-    }
-    
-    for (int i = 0; i < 4; i++)
-    {
+
+        res[i] = 0;
+
+        for (int j = 0; j < f; j++)
+        {
+            for (int k = 0; k < 3; k++)
+            {
+                do{
+                    cin >> fArray[j][k];
+                }while(fArray[j][k] >= 100000 || fArray[j][k] <= 0);
+            }
+        }
+        
+        for (int j = 0; j < f; j++)
+        {
+            res[i] += (fArray[j][0] * fArray[j][2]);
+        }
         
     }
     
-
+    for (int i = 0; i < n; i++)
+        cout << res[i] << endl;
+    
     return 0;
 }
