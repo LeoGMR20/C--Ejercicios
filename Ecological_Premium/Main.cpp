97c974e404481a98801ece8d93cef6a71de4c757
LeoGMR20 <leo250703@gmail.com>
2022-10-07 21:15:28 -0400

    Commit 07/10/2022 20:47 (Leandro Morales) -> avance (aun con errores) de 'Ecological_Premium'

--- Ecological_Premium/Main.cpp
+++ Ecological_Premium/Main.cpp
@@ -4,6 +4,31 @@ using namespace std;
 
 int main()
 {
+    int n, f, fy, a, fef;
+
+    do{
+        cin >> n;
+    }while(n >= 20 || n <= 0);
+
+    for (int i = 0; i < n; i++)
+    {
+        do{
+            cin >> f;
+        }while(f >= 20 || f <= 0);
+
+        int **fArray = new int*[4];
+
+        for(int j = 0; j < 4; j++)
+        {
+            fArray
+        }
+    }
     
+    for (int i = 0; i < 4; i++)
+    {
+        
+    }
+    
+
     return 0;
 }
