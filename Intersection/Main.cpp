2c094030957839821be34d6178f6a3fea235af2c
LeoGMR20 <leo250703@gmail.com>
2023-08-24 13:26:55 -0400

    Commit 13:26 24/08/2023 - Competencia

---
+++ Intersection/Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    long x1, y1, r1, x2, y2, r2;
+
+    while (true)
+    {
+        scanf("%ld %ld %ld %ld %ld %ld", &x1, &y1, &r1, &x2, &y2, &r2);
+    }
+
+    return 0;
+}
