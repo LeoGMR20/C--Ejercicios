4dd1d73e500682166919097c1f916155240f3469
LeoGMR20 <leo250703@gmail.com>
2023-08-24 23:32:47 -0400

    Commit 23:32 24/08/2023

--- Intersection/Main.cpp
+++ Intersection/Main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdio.h>
+#include <math.h>
 
 using namespace std;
 
@@ -10,6 +11,12 @@ int main()
     while (true)
     {
         scanf("%ld %ld %ld %ld %ld %ld", &x1, &y1, &r1, &x2, &y2, &r2);
+
+        if (x1 + y1 + r1 + x2 + y2 + r2 == 0)
+            break;
+
+        sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2)) < r1 + r2 ? cout << "SI" : cout << "NO";
+        cout << endl;
     }
 
     return 0;
